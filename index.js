a393e20ccbe6362f23902ef47ae8b2fc45a13eb1
Michael Swanson <michael@mswanson.me>
2018-09-19 20:24:45 -0700

    added notes for code clarity

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-let listContent = '';
 let LISTITEMS = [];
 
 const MONTHS = [
@@ -16,12 +15,16 @@ const MONTHS = [
   'December'
 ];
 
-if (localStorage.length > 0){
-  for (let i = 0; i < localStorage.length; i++){
-    LISTITEMS.push(JSON.parse(localStorage.getItem(i)));
-  }
-};
+/* Read existing list from localStorage, if one exists */
+function loadExistingList(){
+  if (localStorage.length > 0){
+    for (let i = 0; i < localStorage.length; i++){
+      LISTITEMS.push(JSON.parse(localStorage.getItem(i)));
+    }
+  };
+}
 
+/* Synchronizes localStorage with volatile LISTITEMS array */
 function syncLocalStorage(){
   localStorage.clear();
   for (let i = 0; i < LISTITEMS.length; i++){
@@ -31,6 +34,7 @@ function syncLocalStorage(){
   };
 }
 
+/* Retrieves, formats, and renders date and time */
 function renderDateTime(){
   let time = new Date();
   let year = time.getFullYear();
@@ -52,29 +56,31 @@ function renderDateTime(){
   $('.date').html(`${month} ${day}, ${year} <br /> ${hours}:${mins} ${amPm}`);
 }
 
+/* Creates new item object and pushes it to LISTITEMS array */
 function addNewItem(item){
-  let o = new Object();
-  o.item = item;
-  o.checked = false;
-  LISTITEMS.push(o);
+  let itemObject = new Object();
+  itemObject.item = item;
+  itemObject.checked = false;
+  LISTITEMS.push(itemObject);
 }
 
+/* Aggregates LISTITEMS content into an HTML string */
 function populateList(){
-  listContent = '';
+  let listContent = '';
   
   for (let i = 0; i < LISTITEMS.length; i++){
     listContent = listContent + `<li class="list-item ${LISTITEMS[i].checked ? 'strikethrough' : ''}">${LISTITEMS[i].item}<span class="btn-group"><button type="button" class="complete-item fas fa-check"></button><button type="button" class="delete-item fas fa-trash"></button></span></li>`;
   };
   
-  console.log('populated...');
   return listContent;
 }
 
+/* Renders the HTML string (usually from populateList) to the page */
 function renderList(content){
   $('#itemList').html(content);
-  console.log('rendered...');
 };
 
+/* Initializes event listeners */
 function initEventListeners(){  
   $('main').on('click', '.newItem', function(event){
     event.preventDefault();
@@ -86,20 +92,17 @@ function initEventListeners(){
       renderList(populateList());
       $('input').val('');
     };
-    console.log('add item clicked...');
   });
 
   $('main').on('click', '.complete-item', function(event){
     let strikeIndex = $(event.target).closest('li').index();
     LISTITEMS[strikeIndex].checked = !LISTITEMS[strikeIndex].checked;
-    console.log('item completed...');
     renderList(populateList());
   });
 
   $('main').on('click', '.delete-item', function(event){
     let spliceIndex = $(event.target).closest('li').index();
     LISTITEMS.splice(spliceIndex, 1);
-    console.log('item deleted...');
     renderList(populateList());
   });
 
@@ -110,8 +113,13 @@ function initEventListeners(){
   };
 };
 
+/* The main function for the page
+*  Loads existing list, inits event handlers
+*  Init intervals for updating time, periodically saving list
+*  Performs the initial list render
+*/
 function toDoLoop(){
-  console.log('initializing event listeners...');
+  loadExistingList();
   initEventListeners();
   setInterval(renderDateTime, 1000);
   setInterval(syncLocalStorage, 600000);
